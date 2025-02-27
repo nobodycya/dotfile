8bbdeeabdf1a831797f8404afd37c996670713bc
raindrop0123 <139353546+raindrop0123@users.noreply.github.com>
2025-02-27 19:30:33 +0800

    Update wezterm.lua

--- cross/wezterm/wezterm.lua
+++ cross/wezterm/wezterm.lua
@@ -9,19 +9,19 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.font_size = 9.00
   config.font = wezterm.font("JetBrainsMono Nerd Font")
   config.default_prog = { "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" }
-  config.window_background_opacity = 0.95
+  config.window_background_opacity = 0.90
 elseif wezterm.target_triple == "aarch64-apple-darwin" then
   config.font_size = 13.00
   config.font = wezterm.font("JetBrains Mono")
   config.default_prog = { "zsh", "-l" }
-  config.window_background_opacity = 0.95
+  config.window_background_opacity = 0.90
 elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
   config.font_size = 12.00
   config.font = wezterm.font("JetBrainsMono Nerd Font")
   config.default_prog = { "bash", "-l" }
-  config.window_background_opacity = 0.85
+  config.window_background_opacity = 0.80
 end
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "Vs Code Dark+ (Gogh)"
 config.line_height = 1.00
 config.default_cursor_style = "BlinkingBlock"
 config.animation_fps = 1
